317d2f3c95567397d10e45362a9fda015b8862b5
Jose Martin <j.antonio.martin.fdez@gmail.com>
2023-11-04 15:11:00 +0100

    TDD exercise

--- app/src/main/kotlin/gradle/kotlin/latest/App.kt
+++ app/src/main/kotlin/gradle/kotlin/latest/App.kt
@@ -1,12 +1,39 @@
 package gradle.kotlin.latest
 
-class App {
+class App(val matrix: Map<Int, Map<Int, Boolean>>) {
     val greeting: String
         get() {
             return "Hello World!"
         }
+
+    fun renderGame(): MutableMap<Int, MutableMap<Int, String>> {
+        val renderedMatrix = mutableMapOf<Int, MutableMap<Int, String>>()
+
+        matrix.forEach { row, columnMap ->
+            renderedMatrix[row] = mutableMapOf()
+            columnMap.forEach { column, value ->
+                if (value) {
+                    renderedMatrix[row]?.set(column, "*")
+                } else {
+                    renderedMatrix[row]?.set(column, ".")
+                }
+            }
+        }
+
+        return renderedMatrix
+    }
+
+    fun printGame()
+    {
+        renderGame().forEach { row, columnMap ->
+            columnMap.forEach { column, value ->
+                print(value)
+            }
+            println()
+        }
+    }
 }
 
 fun main() {
-    println(App().greeting)
+    println(App(emptyMap()).greeting)
 }
--- app/src/test/kotlin/gradle/kotlin/latest/AppTest.kt
+++ app/src/test/kotlin/gradle/kotlin/latest/AppTest.kt
@@ -6,7 +6,33 @@ import org.junit.jupiter.api.Test
 class AppTest {
     @Test
     fun appHasAGreeting() {
-        val classUnderTest = App()
+        val classUnderTest = App(emptyMap())
         assertThat(classUnderTest.greeting).isNotNull
+    }
+
+    @Test
+    fun `given a matrix when printing it then it should show died cells as dots and alives ones as asterisk` (){
+        val matrix = mutableMapOf (
+            0 to mutableMapOf(0 to false, 1 to false, 2 to false, 3 to false, 4 to false, 5 to false, 6 to false, 7 to false),
+            1 to mutableMapOf(0 to false, 1 to false, 2 to false, 3 to true, 4 to true, 5 to false, 6 to false, 7 to false),
+            2 to mutableMapOf(0 to false, 1 to false, 2 to false, 3 to true, 4 to true, 5 to false, 6 to false, 7 to false),
+            3 to mutableMapOf(0 to false, 1 to false, 2 to false, 3 to false, 4 to false, 5 to false, 6 to false, 7 to false)
+        )
+
+
+        val classUnderTest = App(matrix)
+        val actualMatrix = classUnderTest.renderGame()
+
+        matrix.forEach { row, columnMap ->
+            columnMap.forEach { column, value ->
+                if (value) {
+                    assertThat(actualMatrix[row]!![column]).isEqualTo("*")
+                } else {
+                    assertThat(actualMatrix[row]!![column]).isEqualTo(".")
+                }
+            }
+        }
+
+        classUnderTest.printGame()
     }
 }
